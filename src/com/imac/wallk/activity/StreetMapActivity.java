98814c3dab5568d4afaf6c7321e864ba0b812b5f
Cecilia Lejeune <cecilia.lejeu@gmail.com>
2014-02-25 11:03:04 +0100

    Displaying a GoogleMap test in the StreetMapActivity Caution : the API Key of the Manifest is mine and might have to be replaced by yours

--- src/com/imac/wallk/activity/StreetMapActivity.java
+++ src/com/imac/wallk/activity/StreetMapActivity.java
@@ -6,19 +6,37 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.imac.wallk.R;
 import com.parse.ParseUser;
 
 public class StreetMapActivity extends Activity {
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		// hide the title in the action bar
-        getActionBar().setDisplayShowTitleEnabled(false);
-		super.onCreate(savedInstanceState);
-		
-		setContentView(R.layout.activity_map);
-	}
-	
+
+	    @Override
+	    protected void onCreate(Bundle savedInstanceState) {
+	        super.onCreate(savedInstanceState);
+	        setContentView(R.layout.activity_map);
+
+	        // Get a handle to the Map Fragment
+	        GoogleMap map = ((MapFragment) getFragmentManager()
+	                .findFragmentById(R.id.map)).getMap();
+
+	        LatLng sydney = new LatLng(-33.867, 151.206);
+
+	        map.setMyLocationEnabled(true);
+	        map.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 13));
+
+	        map.addMarker(new MarkerOptions()
+	                .title("Sydney")
+	                .snippet("The most populous city in Australia.")
+	                .position(sydney));
+	    }
+
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		if(ParseUser.getCurrentUser() != null){
@@ -29,6 +47,29 @@ public class StreetMapActivity extends Activity {
 		return true;
 	}
 
+//	/** Google Map creation + Security check to ensure the map can be instantiated **/
+//	private void setUpMapIfNeeded() {
+//	    // Do a null check to confirm that we have not already instantiated the map.
+//	    if (mMap == null) {
+//	        mMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map))
+//	                            .getMap();
+////	        //Set up the options for the map
+////	        GoogleMapOptions options = new GoogleMapOptions();
+////	        options.mapType(GoogleMap.MAP_TYPE_SATELLITE)
+////	        		.compassEnabled(false)
+////	        		.rotateGesturesEnabled(false)
+////	        		.tiltGesturesEnabled(false);
+//
+//	        
+//	        // Check if we were successful in obtaining the map.
+//	        if (mMap != null) {
+//	            // The Map is verified. It is now safe to manipulate the map.
+//
+//	        }
+//	    }
+//	}
+
+	
 	/*
 	 * Handler function for ActionBar's buttons click event
 	 */
@@ -90,4 +131,5 @@ public class StreetMapActivity extends Activity {
 		
 		return super.onOptionsItemSelected(item);
 	}
+
 }
