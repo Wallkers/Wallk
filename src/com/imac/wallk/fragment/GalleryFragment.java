6a91aaebf10545c2095b5f930c1fc7a4dc9ee8f6
Artourn <arthurtourneret@gmail.com>
2014-03-26 17:49:15 +0100

    Can display an image when clicked

--- src/com/imac/wallk/fragment/GalleryFragment.java
+++ src/com/imac/wallk/fragment/GalleryFragment.java
@@ -8,6 +8,9 @@ import com.imac.wallk.R;
 import com.imac.wallk.adapter.FavoriteArtworkAdapter;
 import com.imac.wallk.adapter.UserArtworkAdapter;
 import com.imac.wallk.adapter.AllArtworkAdapter;
+import com.parse.GetDataCallback;
+import com.parse.ParseFile;
+import com.parse.ParseImageView;
 import com.parse.ParseQueryAdapter;
 import com.parse.ParseQueryAdapter.OnQueryLoadListener;
 import com.parse.ParseException;
@@ -19,6 +22,7 @@ import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.util.Log;
 import android.app.ProgressDialog;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
 
@@ -26,6 +30,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
@@ -38,6 +44,7 @@ public class GalleryFragment extends ListFragment {
 	private FavoriteArtworkAdapter favoritesAdapter;
 	private ProgressDialog progressDialog = null;
 	private ListView listOfPictures =  null;
+	private ParseImageView parseImgView;
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -45,24 +52,14 @@ public class GalleryFragment extends ListFragment {
 		
 		super.onCreate(savedInstanceState);
 		View v = inflater.inflate(R.layout.listfragment_gallery, container, false);
-		
+		parseImgView = (ParseImageView) v.findViewById(R.id.picture_to_display);
+
 		if(listOfPictures == null){
 			listOfPictures = (ListView)v.findViewById(android.R.id.list);
 		}
 		
-		listOfPictures.setOnItemClickListener(new OnItemClickListener() {
-		public void onItemClick(AdapterView<?> parent, View view,int position, long id) 
-	    {
-	      String selectedFromList = (listOfPictures.getItemAtPosition(position).getClass().toString());
-	      Toast.makeText(
-					getActivity().getApplicationContext(),
-					"type: " + selectedFromList,
-					Toast.LENGTH_SHORT).show();
-
-	    }});
-	    Log.d("coucou", "toi");
 		mainAdapter = new AllArtworkAdapter(this.getActivity());
-
+		
 		//adapters allow to sort pictures
 		favoritesAdapter = new FavoriteArtworkAdapter(this.getActivity());
 		//sort pictures by user
@@ -83,7 +80,6 @@ public class GalleryFragment extends ListFragment {
 		progressDialog.setMessage("Charging all pictures. Please wait.");
 		progressDialog.show();
 		mainAdapter.loadObjects();
-		
 		mainAdapter.addOnQueryLoadListener(new OnQueryLoadListener<Artwork>() {
 			   public void onLoading() {
 			     // Trigger any "loading" UI
@@ -91,6 +87,23 @@ public class GalleryFragment extends ListFragment {
 			
 				@Override
 				public void onLoaded(List<Artwork> objects, Exception e) {
+					Log.d("COUNT", Integer.toString(listOfPictures.getCount()));
+					listOfPictures.setOnItemClickListener(new OnItemClickListener() {
+						public void onItemClick(AdapterView<?> parent, View view,int position, long id) 
+					    {
+					      Artwork artworkSelected = (Artwork)listOfPictures.getItemAtPosition(position);
+					      //String title = artworkSelected.getTitle();
+					      ParseFile picture = artworkSelected.getPhotoFile();
+							if (picture != null) {
+								byte[] pictureData = null;
+								try {
+									pictureData = picture.getData();
+								} catch (ParseException e) {
+									e.printStackTrace();
+								}
+								parseImgView.setImageBitmap(BitmapFactory.decodeByteArray(pictureData, 0, pictureData.length));
+							}
+						    }});
 					progressDialog.dismiss();
 				}
 		});
